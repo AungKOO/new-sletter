c951d36cc1b58a9ecde93d91b2b4c21e0963e8bb
Aung KO <aungko.uit@gmail.com>
2021-07-10 01:04:39 +0630

    finish UI

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express'); 
-const bodyParser = require('body-parser');
-const request = require('request');
-const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    console.log(firstName, lastName, email);
+})
 app.listen(3000, () => console.log("Server is running at port 3000"));
